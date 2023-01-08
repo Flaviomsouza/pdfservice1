e18466e9a0cded0ab2f55d289ddd12d07913eaeb
Caléb Rangel Porto <calebbds@gmail.com>
2023-01-08 11:30:20 -0300

    teste cronjob email

---
+++ email_tester.py
@@ -0,0 +1,18 @@
+import smtplib
+import email.message
+
+
+email_body = 'Testando cronjobs'
+
+msg = email.message.Message()
+msg['Subject'] = 'TESTE CRONJOB'
+msg['From'] = 'calebbds@gmail.com'
+msg['To'] = 'calebporto@hotmail.com'
+password = 'kpxrnsqdsvrtvnka'
+msg.add_header('Content-Type', 'text/html')
+msg.set_payload(email_body)
+
+s = smtplib.SMTP('smtp.gmail.com: 587')
+s.starttls()
+s.login(msg['From'], password)
+s.sendmail(msg['From'], msg['To'], msg.as_string().encode('utf-8'))
